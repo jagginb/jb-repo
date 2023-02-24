645e1d8644c45985716a20df2b25e12cdaba1713
jagginb <jagginb@gmail.com>
2023-02-24 15:14:31 +0000

    dt.sh file is edited and added

--- dt.sh
+++ dt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-	echo "Hello"
-	echo "Current date is `date`"
-	echo "User is `whoami`"
-	echo "Current direcotry `pwd`"
+	echo "Hi"
+	echo "present date is `date`"
+	echo "User in the server is `whoami`"
+	echo "Current working direcotry `pwd`"
